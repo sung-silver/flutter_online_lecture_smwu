faa4bede266a68f67d4e7c3ecb3ed2eafc1dc321
sung-silver <jinna0319@gmail.com>
2025-01-14 15:21:19 +0900

    feat: 2일차 - container 예제

--- lib/screen/container_screen.dart
+++ lib/screen/container_screen.dart
@@ -7,7 +7,25 @@ class ContainerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Container"),
+      appBar: AppBar(title: Text("Container"),),
+      body: Container(
+        width: 300,
+        height: 300,
+        color: Colors.grey,
+        padding: EdgeInsets.all(10),
+        alignment: Alignment.bottomRight,
+        child: Container(
+          width: 100,
+          height: 100,
+          // 해당 위젯 바깥 여백 추가
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(width: 3, color: Colors.red),
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+      ),
     );
   }
 }

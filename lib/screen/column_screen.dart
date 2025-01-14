70ac2282eb2f55eb1dc86140a41f972758aa00ca
sung-silver <jinna0319@gmail.com>
2025-01-14 16:10:26 +0900

    feat: 2일차 - column 예제

--- lib/screen/column_screen.dart
+++ lib/screen/column_screen.dart
@@ -7,7 +7,36 @@ class ColumnScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Column")
+      appBar: AppBar(title: Text("Column"),),
+      body: Container(
+        color: Colors.grey,
+        width: 300,
+        child: Column(
+          // 현업에서는 start, center, end, spaceBetween 많이 사용
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // stretch: 가능한 교차축 사이즈를 최대한으로 크게 함 -> 자식 사이즈가 무시됨
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Container 3개 추가
+            // 가로, 세로 100, color는 각각 파란색/빨간색/초록색
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green
+            )
+          ],
+        ),
+      )
     );
   }
 }

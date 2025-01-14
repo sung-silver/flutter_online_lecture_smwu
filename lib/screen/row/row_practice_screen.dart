00b8e7c0101f0ec17f8cf022f989703645f99291
sung-silver <jinna0319@gmail.com>
2025-01-14 16:32:47 +0900

    feat: 2일차 - row 실습

---
+++ lib/screen/row/row_practice_screen.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+
+class RowPracticeScreen extends StatelessWidget {
+  const RowPracticeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Row 실습")),
+      body: Container(
+        color: Colors.purple,
+        height: 300,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.green
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

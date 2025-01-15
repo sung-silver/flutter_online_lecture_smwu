33ebbd0eaeeb229e1f647939e5b0321c45234f99
sung-silver <jinna0319@gmail.com>
2025-01-15 16:01:30 +0900

    feat: 3일차 - Text 예제

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -6,6 +6,7 @@ import 'package:online_lecture_smwu/screen/container/container_practice_screen.d
 import 'package:online_lecture_smwu/screen/container/container_screen.dart';
 import 'package:online_lecture_smwu/screen/row/row_practice_screen.dart';
 import 'package:online_lecture_smwu/screen/row/row_screen.dart';
+import 'package:online_lecture_smwu/screen/text/text_screen.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -102,6 +103,18 @@ class ButtonScreen extends StatelessWidget {
                   ));
                 },
                 child: Text("Column, Row 심화")),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return TextScreen();
+                    },
+                  ));
+                },
+                child: Text("Text")),
           ],
         ),
       ),

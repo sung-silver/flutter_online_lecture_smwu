cef585afc2a48b77c6a781fa522fd977f97676a0
sung-silver <jinna0319@gmail.com>
2025-01-22 02:38:21 +0900

    feat: 7일차 - 데이터 호출 전 로딩뷰 구현

--- lib/screen/network/member_list_screen.dart
+++ lib/screen/network/member_list_screen.dart
@@ -3,26 +3,40 @@ import 'package:flutter/material.dart';
 
 class MemberListScreen extends StatefulWidget {
   const MemberListScreen({super.key});
+
   @override
   State<MemberListScreen> createState() => _MemberListScreenState();
 }
+
 class _MemberListScreenState extends State<MemberListScreen> {
-  Dio dio = Dio(BaseOptions(baseUrl: "https://244b-110-8-126-227.ngrok-free.app"));
+  bool loading = true;
+  Dio dio =
+      Dio(BaseOptions(baseUrl: "https://244b-110-8-126-227.ngrok-free.app"));
+
   @override
   void initState() {
     // TODO: implement initState
     getDate();
     super.initState();
   }
+
   Future<void> getDate() async {
     Response response = await dio.get("/api/v1/member/all");
-    print(response.statusCode.toString());
-    print(response.data);
+    loading = false;
+    setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("member list")),
+      body: loading
+          ? CircularProgressIndicator()
+          : SingleChildScrollView(
+              child: Column(
+                children: [Text("data 호출 완료")],
+              ),
+            ),
     );
   }
 }

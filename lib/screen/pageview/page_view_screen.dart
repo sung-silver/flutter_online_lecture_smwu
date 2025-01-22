6b4c5a12cafbd6ca6b5bf43d5d95634d87bfc481
sung-silver <jinna0319@gmail.com>
2025-01-22 15:20:00 +0900

    feat: 8일차 - page_view 심화 예제

--- lib/screen/pageview/page_view_screen.dart
+++ lib/screen/pageview/page_view_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PageViewScreen extends StatefulWidget {
@@ -9,23 +8,57 @@ class PageViewScreen extends StatefulWidget {
 }
 
 class _PageViewScreenState extends State<PageViewScreen> {
+  final PageController pageController = PageController();
+
+  double opacity = 0;
+
+  @override
+  void initState() {
+    pageController.addListener(
+          () {
+        opacity = pageController.page! * 0.3;
+        setState(() {});
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("PageView")),
+      appBar: AppBar(
+        title: Text("PageView"),
+      ),
       body: Column(
         children: [
+          Opacity(
+            opacity: opacity,
+            child: Container(
+              color: Colors.black,
+              height: 50,
+            ),
+          ),
           Expanded(
             child: PageView(
+              controller: pageController,
+              onPageChanged: (value) {
+                print("value: $value");
+              },
               children: [
-                Container(color: Colors.red,),
-                Container(color: Colors.blue,),
-                Container(color: Colors.green,),
+                Container(color: Colors.red),
+                Container(color: Colors.blue),
+                Container(color: Colors.green),
               ],
             ),
           ),
         ],
-      )
+      ),
     );
   }
 }
